30c056d0877f54ff40746e92622093331a4ed4fa
Eric Johnson <ericj@mips.com>
2013-02-08 11:14:21 -0800

    Add microAptiv UC & UP targets.
    M14KE and M14KEc were renamed microAptiv UC and microAptiv UP.
    Instead of simply renaming in QEMU, aliases are added to allow
    scripts to transition.

--- target-mips/translate_init.c
+++ target-mips/translate_init.c
@@ -323,6 +323,56 @@ static mips_def_t mips_defs[] =
         .mmu_type = MMU_TYPE_FMT,
     },
     {
+        .name = "microAptivUP",
+        .CP0_PRid = 0x00019e00,
+        .CP0_Config0 = MIPS_CONFIG0 | (0x1 << CP0C0_AR) |
+                    (MMU_TYPE_R4000 << CP0C0_MT),
+        .CP0_Config1 = MIPS_CONFIG1 | (15 << CP0C1_MMU) |
+                       (0 << CP0C1_IS) | (3 << CP0C1_IL) | (1 << CP0C1_IA) |
+                       (0 << CP0C1_DS) | (3 << CP0C1_DL) | (1 << CP0C1_DA) |
+                       (0 << CP0C1_CA),
+        .CP0_Config2 = MIPS_CONFIG2,
+        .CP0_Config3 = MIPS_CONFIG3 | (1 << CP0C3_EICW) | (0 << CP0C3_MMAR) |
+                       (0 << CP0C3_MCU) | (0 << CP0C3_ISA_ON_EXC) |
+                       (2 << CP0C3_ISA) | (0 << CP0C3_ULRI) |
+                       (0 << CP0C3_RXI) | (0 << CP0C3_VInt) |
+                       (1 << CP0C3_DSP2P) | (1 << CP0C3_DSPP),
+        .CP0_LLAddr_rw_bitmask = 0,
+        .CP0_LLAddr_shift = 4,
+        .SYNCI_Step = 32,
+        .CCRes = 2,
+        .CP0_Status_rw_bitmask = 0x1378FF17,
+        .SEGBITS = 32,
+        .PABITS = 32,
+        .insn_flags = CPU_MIPS32R2 | ASE_MICROMIPS | ASE_DSP | ASE_DSPR2,
+        .mmu_type = MMU_TYPE_R4000,
+    },
+    {
+        .name = "microAptivUC",
+        .CP0_PRid = 0x00019d00,
+        .CP0_Config0 = MIPS_CONFIG0 | (0x1 << CP0C0_AR) |
+                       (MMU_TYPE_FMT << CP0C0_MT),
+        .CP0_Config1 = MIPS_CONFIG1 |
+                       (0 << CP0C1_IS) | (0 << CP0C1_IL) | (0 << CP0C1_IA) |
+                       (0 << CP0C1_DS) | (0 << CP0C1_DL) | (0 << CP0C1_DA) |
+                       (1 << CP0C1_CA),
+        .CP0_Config2 = MIPS_CONFIG2,
+        .CP0_Config3 = MIPS_CONFIG3 | (1 << CP0C3_EICW) | (0 << CP0C3_MMAR) |
+                       (0 << CP0C3_MCU) | (0 << CP0C3_ISA_ON_EXC) |
+                       (2 << CP0C3_ISA) | (0 << CP0C3_ULRI) |
+                       (0 << CP0C3_RXI) | (0 << CP0C3_VInt) |
+                       (1 << CP0C3_DSP2P) | (1 << CP0C3_DSPP),
+        .CP0_LLAddr_rw_bitmask = 0,
+        .CP0_LLAddr_shift = 4,
+        .SYNCI_Step = 32,
+        .CCRes = 2,
+        .CP0_Status_rw_bitmask = 0x1358FF17,
+        .SEGBITS = 32,
+        .PABITS = 32,
+        .insn_flags = CPU_MIPS32R2 | ASE_MICROMIPS | ASE_DSP | ASE_DSPR2,
+        .mmu_type = MMU_TYPE_FMT,
+    },
+    {
         .name = "24Kc",
         .CP0_PRid = 0x00019300,
         .CP0_Config0 = MIPS_CONFIG0 | (0x1 << CP0C0_AR) |
