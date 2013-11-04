0ca2cef7fe588a287425d2ec5e5d28918a57b1f2
Leon Alrae <leon.alrae@imgtec.com>
2013-11-04 17:43:04 +0000

    Add 40-bit PAE support for MIPS32
    Fix for mmu_*_of_3 failing tests
    
    On TLB fill PageMask affects values of VPN and PFN. If given mask is not valid, then values are set to 0.
    
    Fix VPN EntryHi mask and CP0C4_IE
    
    Fix a bug causing upper 32-bits of EntryLo1 to be updated unexpectedly

--- target-mips/helper.h
+++ target-mips/helper.h
@@ -86,6 +86,16 @@ DEF_HELPER_1(dmfc0_tcschedule, tl, env)
 DEF_HELPER_1(dmfc0_tcschefback, tl, env)
 DEF_HELPER_1(dmfc0_lladdr, tl, env)
 DEF_HELPER_2(dmfc0_watchlo, tl, env, i32)
+#else
+DEF_HELPER_0(mfhc0_entrylo0, tl)
+DEF_HELPER_0(mfhc0_entrylo1, tl)
+DEF_HELPER_0(mfhc0_taglo, tl)
+DEF_HELPER_0(mfhc0_lladdr, tl)
+
+DEF_HELPER_1(mthc0_entrylo0, void, tl)
+DEF_HELPER_1(mthc0_entrylo1, void, tl)
+DEF_HELPER_1(mthc0_taglo, void, tl)
+DEF_HELPER_1(mthc0_lladdr, void, tl)
 #endif /* TARGET_MIPS64 */
 
 DEF_HELPER_2(mtc0_index, void, env, tl)
