8e716a44f0ba169891ac2104504d577d3b187f4c
Leon Alrae <leon.alrae@imgtec.com>
2014-08-27 11:05:18 +0100

    target-mips: add BadInstr and BadInstrP support
    BadInstr Register (CP0 Register 8, Select 1)
    The BadInstr register is a read-only register that capture the most recent
    instruction which caused an exception.
    
    BadInstrP Register (CP0 Register 8, Select 2)
    The BadInstrP register contains the prior branch instruction, when the
    faulting instruction is in a branch delay slot.
    
    Using error_code to indicate whether AdEL or TLBL was triggered during
    instruction fetch, in this case BadInstr is not updated as valid instruction
    word is not available.
    
    Signed-off-by: Leon Alrae <leon.alrae@imgtec.com>

--- target-mips/op_helper.c
+++ target-mips/op_helper.c
@@ -2261,10 +2261,23 @@ static void QEMU_NORETURN do_unaligned_access(CPUMIPSState *env,
 #include "exec/softmmu_template.h"
 
 static void do_unaligned_access(CPUMIPSState *env, target_ulong addr,
-                                int is_write, int is_user, uintptr_t retaddr)
+                                int access_type, int is_user, uintptr_t retaddr)
 {
+    int error_code = 0;
+    int excp;
+
     env->CP0_BadVAddr = addr;
-    do_raise_exception(env, (is_write == 1) ? EXCP_AdES : EXCP_AdEL, retaddr);
+
+    if (access_type == MMU_DATA_STORE) {
+        excp = EXCP_AdES;
+    } else {
+        excp = EXCP_AdEL;
+        if (access_type == MMU_INST_FETCH) {
+            error_code |= EXCP_INST_NOTAVAIL;
+        }
+    }
+
+    do_raise_exception_err(env, excp, error_code, retaddr);
 }
 
 void tlb_fill(CPUState *cs, target_ulong addr, int is_write, int mmu_idx,
