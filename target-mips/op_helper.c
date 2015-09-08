990b0bd7fb373f2d406beb71cd6321e6b3c2c0fb
Alex Smith <alex.smith@imgtec.com>
2015-09-08 11:34:11 +0100

    target-mips: Fix RDHWR on CP0.Count
    For RDHWR on the CP0.Count register, env->CP0_Count was being returned.
    This value is a delta against the QEMU_CLOCK_VIRTUAL clock, not the
    correct current value of CP0.Count. Use cpu_mips_get_count() instead.
    
    Signed-off-by: Alex Smith <alex.smith@imgtec.com>
    Cc: Aurelien Jarno <aurelien@aurel32.net>
    Cc: Leon Alrae <leon.alrae@imgtec.com>
    Reviewed-by: Leon Alrae <leon.alrae@imgtec.com>
    Signed-off-by: Leon Alrae <leon.alrae@imgtec.com>

--- target-mips/op_helper.c
+++ target-mips/op_helper.c
@@ -2351,7 +2351,11 @@ target_ulong helper_rdhwr_synci_step(CPUMIPSState *env)
 target_ulong helper_rdhwr_cc(CPUMIPSState *env)
 {
     check_hwrena(env, 2);
-    return env->CP0_Count;
+#ifdef CONFIG_USER_ONLY
+        return env->CP0_Count;
+#else
+        return (int32_t)cpu_mips_get_count(env);
+#endif
 }
 
 target_ulong helper_rdhwr_ccres(CPUMIPSState *env)
