2484c471e18f0163e4d0f1cf4267e2051629e70f
Yongbok Kim <yongbok.kim@imgtec.com>
2015-10-16 16:27:28 +0100

    mips: fix map_vpe to indicate not-mapped
    The internal value -1 is now indicating the IRQ is not mapped to any VP.
    
    Signed-off-by: Yongbok Kim <yongbok.kim@imgtec.com>

--- hw/intc/mips_gic.c
+++ hw/intc/mips_gic.c
@@ -302,7 +302,11 @@ static uint64_t gic_read(void *opaque, hwaddr addr, unsigned size)
     /* Global Interrupt Map SrcX to VPE register */
     if (addr >= GIC_SH_MAP0_VPE31_0_OFS && addr <= GIC_SH_MAP255_VPE63_32_OFS) {
         int irq_src = (addr - GIC_SH_MAP0_VPE31_0_OFS) / 32;
-        ret = 1 << (gic->irq_state[irq_src].map_vpe);
+        if ((gic->irq_state[irq_src].map_vpe) >= 0) {
+            ret = 1 << (gic->irq_state[irq_src].map_vpe);
+        } else {
+            ret = 0;
+        }
         return ret;
     }
 
@@ -443,7 +447,7 @@ static void gic_write(void *opaque, hwaddr addr, uint64_t data, unsigned size)
     }
     if (addr >= GIC_SH_MAP0_VPE31_0_OFS && addr <= GIC_SH_MAP255_VPE63_32_OFS) {
         int irq_src = (addr - GIC_SH_MAP0_VPE31_0_OFS) / 32;
-        gic->irq_state[irq_src].map_vpe = (data) ? ctz64(data) : 0;
+        gic->irq_state[irq_src].map_vpe = (data) ? ctz64(data) : -1;
     }
 
     /* VPE-Local Register */
@@ -481,7 +485,7 @@ static void gic_reset(void *opaque)
         gic->irq_state[i].enabled        = false;
         gic->irq_state[i].pending        = false;
         gic->irq_state[i].map_pin        = GIC_MAP_TO_PIN_MSK;
-        gic->irq_state[i].map_vpe        = 0;
+        gic->irq_state[i].map_vpe        = -1;
     }
 }
 
