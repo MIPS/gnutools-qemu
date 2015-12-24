5fd4c8eb6a834956c3d41ca058b2230c2ef8e079
Petros Angelatos <petrosagg@resin.io>
2015-12-24 14:43:17 -0800

    linux-user: add option to intercept execve() syscalls
    In order for one to use QEMU user mode emulation under a chroot, it is
    required to use binfmt_misc. This can be avoided by QEMU never doing a
    raw execve() to the host system.
    
    Introduce a new option, -execve, that uses the current QEMU interpreter
    to intercept execve().
    
    qemu_execve() will prepend the interpreter path , similar to what
    binfmt_misc would do, and then pass the modified execve() to the host.
    
    It is necessary to parse hashbang scripts in that function otherwise
    the kernel will try to run the interpreter of a script without QEMU and
    get an invalid exec format error.
    
    Modified by Vasileios Kalintiris in order to pass qemu's environment variables
    into execve() calls.
    
    Signed-off-by: Petros Angelatos <petrosagg@resin.io>

--- linux-user/qemu.h
+++ linux-user/qemu.h
@@ -145,6 +145,7 @@ void task_settid(TaskState *);
 void stop_all_tasks(void);
 void resume_all_tasks(void);
 extern const char *qemu_uname_release;
+extern const char *qemu_execve_path;
 extern unsigned long mmap_min_addr;
 
 /* ??? See if we can avoid exposing so much of the loader internals.  */
