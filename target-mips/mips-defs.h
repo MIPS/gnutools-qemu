e40cd6eb332c58dbe25b4e5a7b6459585d722b8c
Yongbok Kim <yongbok.kim@imgtec.com>
2016-11-17 15:59:01 +0000

    target-mips: add microMIPS++ OPCODE table
    Add microMIPS++ OPCODE table and define MIPS32R7.

--- target-mips/mips-defs.h
+++ target-mips/mips-defs.h
@@ -35,6 +35,7 @@
 #define   ISA_MIPS64R5  0x00001000
 #define   ISA_MIPS32R6  0x00002000
 #define   ISA_MIPS64R6  0x00004000
+#define   ISA_MIPS32R7  0x00008000
 
 /* MIPS ASEs. */
 #define   ASE_MIPS16    0x00010000
@@ -46,6 +47,8 @@
 #define   ASE_SMARTMIPS 0x00400000
 #define   ASE_MICROMIPS 0x00800000
 #define   ASE_MSA       0x01000000
+
+#define   UMIPS_DEC_X7  0x02000000
 
 /* Chip specific instructions. */
 #define		INSN_LOONGSON2E  0x20000000
@@ -82,6 +85,9 @@
 /* MIPS Technologies "Release 6" */
 #define CPU_MIPS32R6 (CPU_MIPS32R5 | ISA_MIPS32R6)
 #define CPU_MIPS64R6 (CPU_MIPS64R5 | CPU_MIPS32R6 | ISA_MIPS64R6)
+
+/* Imagination Technologies "Release 7" */
+#define CPU_MIPS32R7 (CPU_MIPS32R6 | ISA_MIPS32R7)
 
 /* Strictly follow the architecture standard:
    - Disallow "special" instruction handling for PMON/SPIM.
