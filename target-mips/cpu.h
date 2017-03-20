da26c39dbee778031b468457cfc9e851b74ec29c
Yongbok Kim <yongbok.kim@imgtec.com>
2017-03-20 11:05:00 +0000

    target-mips: implement Global Invalidates
    Implement new Global Invalidate instruction. As QEMU doesn't support
    caches and Virtualization, this implementation only cover the GINVT
    (Global Invalidate TLB) instruction.
    
    Signed-off-by: Yongbok Kim <yongbok.kim@imgtec.com>

--- target-mips/cpu.h
+++ target-mips/cpu.h
@@ -20,6 +20,7 @@ struct r4k_tlb_t {
     target_ulong VPN;
     uint32_t PageMask;
     uint16_t ASID;
+    uint32_t MMID;
     unsigned int G:1;
     unsigned int C0:3;
     unsigned int C1:3;
@@ -308,6 +309,7 @@ struct CPUMIPSState {
 #define CP0GN_VPId 0
     target_ulong CP0_Context;
     target_ulong CP0_KScratch[MIPS_KSCRATCH_NUM];
+    int32_t CP0_MemoryMapID;
     int32_t CP0_PageMask;
     int32_t CP0_PageGrain_rw_bitmask;
     int32_t CP0_PageGrain;
@@ -517,6 +519,10 @@ struct CPUMIPSState {
 #define CP0C5_CV         29
 #define CP0C5_EVA        28
 #define CP0C5_MSAEn      27
+#define CP0C5_ULS        20
+#define CP0C5_CRCP       18
+#define CP0C5_MI         17
+#define CP0C5_GI         15
 #define CP0C5_XNP        13
 #define CP0C5_UFE        9
 #define CP0C5_FRE        8
