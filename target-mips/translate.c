MIPS/gnutools-qemu

commit ad5ed727b53ce6e4a9c0b8db9d25e4d3dab78653

R7 ADDIU32 takes an unsigned 16-bit immediate not signed

target-mips/translate.c
@@ -16842,8 +16842,8 @@ static int decode_micromips32_48_r7_opc(CPUMIPSState *env, DisasContext *ctx)
                 break;
             }
         } else {
-          int16_t imm;
-          imm = (int16_t) extract32(ctx->opcode, 0, 16);
+          uint16_t imm;
+          imm = (uint16_t) extract32(ctx->opcode, 0, 16);
           gen_arith_imm(ctx, OPC_ADDIU, rt, rs, imm);
         }
         break;

commit 2ecda3e3b03e724a0ad65cd0dffa051880806b99

target-mips: Fix nanoMIPS addiu[32] erroneous sign extension
Further to the preceeding commit ad5ed727b53c ("R7 ADDIU32 takes an
unsigned 16-bit immediate not signed"), stop using gen_arith_imm which
takes the immediate as an int16_t parameter. This caused the unsigned
immediate to be cast back to a signed one which was then incorrectly
sign extended to 32 or 64 bits.

Signed-off-by: Paul Burton <paul.burton@imgtec.com>
Fixes: ad5ed727b53c ("R7 ADDIU32 takes an unsigned 16-bit immediate not signed")

target-mips/translate.c
@@ -16842,9 +16842,14 @@ static int decode_micromips32_48_r7_opc(CPUMIPSState *env, DisasContext *ctx)
                 break;
             }
         } else {
-          uint16_t imm;
-          imm = (uint16_t) extract32(ctx->opcode, 0, 16);
-          gen_arith_imm(ctx, OPC_ADDIU, rt, rs, imm);
+            uint16_t imm;
+            imm = (uint16_t) extract32(ctx->opcode, 0, 16);
+            if (rs != 0) {
+                tcg_gen_addi_tl(cpu_gpr[rt], cpu_gpr[rs], imm);
+                tcg_gen_ext32s_tl(cpu_gpr[rt], cpu_gpr[rt]);
+            } else {
+                tcg_gen_movi_tl(cpu_gpr[rt], imm);
+            }
         }
         break;
     case R7_ADDIUPC: