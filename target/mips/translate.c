66f04eaa546ae8ad7d3249b5a889e36c61dbdb36
Yongbok Kim <yongbok.kim@imgtec.com>
2017-06-19 22:16:00 +0100

    mips: Fix wrong boot loader for non-nanoMIPS
    Signed-off-by: Yongbok Kim <yongbok.kim@imgtec.com>

--- target/mips/translate.c
+++ target/mips/translate.c
@@ -23211,6 +23211,18 @@ void cpu_set_exception_base(int vp_index, target_ulong address)
     vp->env.exception_base = address;
 }
 
+bool cpu_supports_nanomips(const char *cpu_model)
+{
+    const mips_def_t *def = cpu_mips_find_by_name(cpu_model);
+    if (!def) {
+        return false;
+    }
+
+    return ((((def->CP0_Config0 >> CP0C0_AR) & 7) == 2) &&
+        (((def->CP0_Config3 >> CP0C3_MMAR) & 7) == 3) &&
+        (((def->CP0_Config3 >> CP0C3_ISA) & 3) == 1));
+}
+
 void cpu_state_reset(CPUMIPSState *env)
 {
     MIPSCPU *cpu = mips_env_get_cpu(env);
