e9bbd9cd016684bd6080769120bbc8d87ef4c264
Zubair Lutfullah Kakakhel <Zubair.Kakakhel@imgtec.com>
2017-07-06 14:08:16 +0100

    MIPS: gic watchdog timer: Fix for reset change upstream
    Signed-off-by: Zubair Lutfullah Kakakhel <Zubair.Kakakhel@imgtec.com>

--- hw/timer/mips_gicwdtimer.c
+++ hw/timer/mips_gicwdtimer.c
@@ -52,7 +52,7 @@ static void gic_vpwdtimer_cb(void *opaque)
             if (vpwdtimer->config & GIC_WDT_CONFIG_WDINTR) {
                 /* Previous interrupt went unacknowledged - reset! */
                 vpwdtimer->config |= GIC_WDT_CONFIG_RESET;
-                qemu_system_reset_request();
+                qemu_system_reset_request(SHUTDOWN_CAUSE_GUEST_RESET);
             }
             /* Fall through */
         case GIC_WDT_CONFIG_PIT:
