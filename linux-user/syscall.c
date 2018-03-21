6230a09404322324ee1b2382815269dfbc7424e3
Aleksandra Karadzic <aleksandra.karadzic@rt-rk.com>
2018-03-21 18:16:07 +0100

    linux-user: Add implementation of signalfd syscall
    Signed-off-by: Aleksandra Karadzic <aleksandra.karadzic@rt-rk.com>

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -43,6 +43,7 @@
 #include <sys/resource.h>
 #include <sys/mman.h>
 #include <sys/swap.h>
+#include <sys/signalfd.h>
 #include <linux/capability.h>
 #include <signal.h>
 #include <sched.h>
@@ -6358,6 +6359,27 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
 #ifdef TARGET_NR_signal
     case TARGET_NR_signal:
         goto unimplemented;
+#endif
+#ifdef TARGET_NR_signalfd
+    case TARGET_NR_signalfd:
+#endif
+#ifdef TARGET_NR_signalfd4
+    case TARGET_NR_signalfd4:
+#endif
+#if defined(TARGET_NR_signalfd) || defined(TARGET_NR_signalfd4)
+        {
+            sigset_t set;
+            p = lock_user(VERIFY_READ, arg2, sizeof(target_sigset_t), 1);
+            if (p == NULL) {
+                goto efault;
+            }
+            target_to_host_sigset(&set, p);
+            unlock_user(p, arg2, 0);
+            ret = get_errno(signalfd(arg1, &set,
+                                     target_to_host_bitmask(arg3,
+                                                            fcntl_flags_tbl)));
+        }
+        break;
 #endif
     case TARGET_NR_acct:
         if (arg1 == 0) {
