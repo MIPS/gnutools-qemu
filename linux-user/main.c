948a2d6f35eaf7cf913f589a6ec27d594ee76d40
Faraz Shahbazker <fshahbazker@wavecomp.com>
2019-09-05 12:23:54 -0700

    linux-user:mips: Skip NaN mode check for soft-float
        Cherry-picked from commit 492f576 on mips_rel/3_0_0/master

--- linux-user/main.c
+++ linux-user/main.c
@@ -4469,8 +4469,9 @@ int main(int argc, char **argv, char **envp)
         if (regs->cp0_epc & 1) {
             env->hflags |= MIPS_HFLAG_M16;
         }
-        if (((info->elf_flags & EF_MIPS_NAN2008) != 0) !=
-            ((env->active_fpu.fcr31 & (1 << FCR31_NAN2008)) != 0)) {
+        if (info->fp_abi != Val_GNU_MIPS_ABI_FP_SOFT
+	    && (((info->elf_flags & EF_MIPS_NAN2008) != 0) !=
+		((env->active_fpu.fcr31 & (1 << FCR31_NAN2008)) != 0))) {
             if ((env->active_fpu.fcr31_rw_bitmask &
                   (1 << FCR31_NAN2008)) == 0) {
                 fprintf(stderr, "ELF binary's NaN mode not supported by CPU\n");
